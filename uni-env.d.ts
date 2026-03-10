b1b318c22822cafe1e36e9ec69f060f04ab30043
zhang-munan <zhangmunan_966@163.com>
2026-03-10 14:50:42 +0800

    chore: Ignore auto-generated declaration files and remove `uni-env.d.ts`.

--- uni-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  /** 环境变量 */
-  readonly VITE_BASE_URL: string
-  /** 环境变量 */
-  readonly VITE_CALLBACK_URL: string
-  /** 环境变量 */
-  readonly VITE_MP_APPID: string
-}
